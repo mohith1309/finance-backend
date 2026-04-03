d9b24f252804f8ef9662078574296c886eca3f7d
mohith1309 <167955561+mohith1309@users.noreply.github.com>
2026-04-03 12:18:06 +0530

    final submission ready

--- server.js
+++ server.js
@@ -11,4 +11,18 @@ app.use("/auth", require("./routes/authRoutes"));
 app.use("/records", require("./routes/recordRoutes"));
 app.use("/dashboard", require("./routes/dashboardRoutes"));
 
+app.get("/", (req, res) => {
+  res.send("Finance Backend Running 🚀");
+});
+
+app.get("/demo", (req, res) => {
+  res.json({
+    message: "Finance Backend Working",
+    sample: {
+      totalIncome: 5000,
+      totalExpense: 2000,
+      net: 3000
+    }
+  });
+});
 app.listen(5000, () => console.log("Server running on port 5000"));
